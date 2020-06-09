4bd1183864ec57bf88725115506a320507de1467
kevinuribe <kevineuv@gmail.com>
2020-06-08 21:55:35 -0500

    Uso de variables y TemplateString

---
+++ consLet.js
@@ -0,0 +1,23 @@
+let nombre = "Kevin Esneider Uribe Villa"
+
+console.log("Hola " + nombre);
+
+//Caracteristicas de Var
+let Edad = 18;
+
+if(Edad >= 18){
+    var esAdulto = "True"
+}
+
+console.log(esAdulto);
+
+//Caracteristicas const
+//no se puede modificar la variabe "const"
+
+const animal = "Perro";
+//animal = "Gato";
+console.log(animal);
+
+const colores = ["Amarillo","Azul"];
+colores.push("Rojo");
+console.log(colores)
---
+++ templateStrings.js
@@ -0,0 +1,5 @@
+const nombre = "Kevin Uribe";
+const edad = 21;
+const pais = "Colombia";
+
+console.log(`Hola, mi nombre es ${nombre}, tengo ${edad} años y vivo en ${pais}`);
