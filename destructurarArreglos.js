3dd52a2ae2b4c532ef12dae570a560770aea191a
kevinuribe <kevineuv@gmail.com>
2020-06-12 20:35:14 -0500

    Destructurando Arreglos

---
+++ destructurarArreglos.js
@@ -0,0 +1,4 @@
+const persona = ['Kevin Uribe',21,'Colombia'];
+
+const mostrarInfo = [nombre,,pais,profesion = "No definida"] = persona => console.log(`Nombre: ${nombre} \nPais: ${pais} \nProfesión: ${profesion}`);
+mostrarInfo(persona);
